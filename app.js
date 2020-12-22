6d74a38b90e5df22a00e2a74a1e8b8095767b468
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2020-12-22 13:17:11 +0630

    gzip

--- app.js
+++ app.js
@@ -68,20 +68,15 @@ app.use(hpp());
 
 app.use("/api", limiter);
 
-const shouldCompress = (req, res) => {
-  if (req.headers["x-no-compression"]) {
-    // Will not compress responses, if this header is present
-    return false;
-  }
-  // Resort to standard compression
-  return compression.filter(req, res);
-};
-app.use(
-  compression({
-    filter: shouldCompress,
-    threshold: 0,
-  })
-);
+// const shouldCompress = (req, res) => {
+//   if (req.headers["x-no-compression"]) {
+//     // Will not compress responses, if this header is present
+//     return false;
+//   }
+//   // Resort to standard compression
+//   return compression.filter(req, res);
+// };
+app.use(compression());
 
 app.get("/", function (req, res, next) {
   res.render("index");
