6a20cbcd0b7f2884102fcc074ca7488845d284fc
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2021-01-15 02:43:53 +0630

    expose: role

--- models/User.js
+++ models/User.js
@@ -39,7 +39,6 @@ const userSchema = new mongoose.Schema(
         values: ["user", "mod", "admin"],
         message: "Invalid user role",
       },
-      select: false,
     },
     confirmedPassword: {
       type: String,
