3446d75f8a3023a6763f7995627271965188d4a4
Kaung-HtetKyaw <kaunghtetkyaw02749@gmail.com>
2021-01-19 16:53:44 +0630

    test

--- services/Storage/cloudinary.js
+++ services/Storage/cloudinary.js
@@ -4,6 +4,11 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 module.exports = async (base64File, options = {}) => {
   return await cloudinary.uploader.upload(base64File, options);
